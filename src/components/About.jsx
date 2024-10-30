bb02a1a04d82e6c1599ea60fda37df6fe675d11f
harlanljones <141967479+harlanljones@users.noreply.github.com>
2024-10-30 17:43:30 -0400

    Small updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import { ArrowRight } from 'lucide-react';
 
 const About = () => {
   return (
-    <section id="about" className="bg-white">
+    <section id="about" className="py-16 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-32">
         <div className="text-center">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
